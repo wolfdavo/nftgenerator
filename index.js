c67fb719af58b501f28d2df203c88bf2102c41a2
David Wolfenden <david@edensavings.com>
2021-10-24 14:50:17 -0700

    Nothing major yet

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const fs = require('fs');
+const myArgs = process.argv.slice(2);
 const { createCanvas, loadImage } = require('canvas');
 const canvas = createCanvas(1000, 1000);
 const ctx = canvas.getContext('2d');
 const { layers, width, height } = require('./input/config');
-const edition = 10;
+const edition = myArgs.length > 0 ? Number(myArgs[0]) : 1;
+
+// Metadata
+var metadata = [];
+var attributes = [];
+var hash = [];
+var decodedHash = [];
 
 const saveLayer = (_canvas, _edition) => {
   fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer('image/png'));
 }
 
+const addMetadata = (_edition) => {
+  let timeStamp = Date.now();
+  let tempMetadata = {
+    hash: hash.join(''),
+    decodedHash: [],
+    edition: '',
+    date: timeStamp,
+    attributes
+  };
+  metadata.push(tempMetadata);
+  attributes = [];
+}
+
+const addAttribute = (_element, _layer) => {
+  let tempAttr = {
+    id: _element.id,
+    layer: _layer.name,
+    name: _element.name,
+    rarity: _element.rarity
+  }
+  attributes.push(tempAttr);
+  hash.push(_layer.id);
+  hash.push(_element.id);
+  decodedHash.push({[_layer.id]: _element.id})
+}
+
 const drawLayer = async (_layer, _edition) => {
   let element = _layer.elements[Math.floor(Math.random() * _layer.elements.length)];
+  addAttribute(element, _layer);
   const image  = await loadImage(`${_layer.location}${element.fileName}`);
   ctx.drawImage(image, _layer.position.x, _layer.position.y, _layer.size.width, _layer.size.height);
   saveLayer(canvas, _edition);
@@ -20,6 +54,7 @@ for (let i = 1; i <= edition; i++) {
   layers.forEach(layer => {
     drawLayer(layer, i);
   })
+  addMetadata(i)
 }
 
 drawLayer()
